e3985c229697a98444b39f9c70b7618ae796aae4
taha <jokerdz380@gmail.com>
2026-03-28 21:59:19 +0100

    hello

--- Client/mc/src/components/home/Home.jsx
+++ Client/mc/src/components/home/Home.jsx
@@ -31,7 +31,7 @@ const Home =({updateMovieReview}) => {
 
     return (
         <>
-        <h2>hello from action</h2>
+        <h2>hello from action and it works </h2>
             {loading ? (
                 <Spinner/>
             ):  (
